1efc1fe88712cd2674cd0859be7e775bfea9ed72
Angel D. Munoz <scythes.lol@gmail.com>
2021-06-08 23:42:49 -0500

    can't say its fixed but looks somewhat better

--- build.ps1
+++ build.ps1
@@ -1,7 +1,16 @@
-Remove-Item Sutil.Shoelace -R -Force
-Remove-Item dist -R -Force
+Remove-Item Sutil.Shoelace -R -Force -ErrorAction Ignore;
+Remove-Item dist -R -Force -ErrorAction Ignore;
 dotnet tool restore
 dotnet run -C Release
+if ($LASTEXITCODE -gt 0) {
+    Exit 1
+}
 dotnet build Sutil.Shoelace 
+if ($LASTEXITCODE -gt 0) {
+    Exit 1
+}
 dotnet fable --cwd Sutil.Shoelace
+if ($LASTEXITCODE -gt 0) {
+    Exit 1
+}
 dotnet pack Sutil.Shoelace -o dist
